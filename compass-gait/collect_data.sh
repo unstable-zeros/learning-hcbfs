d687698ed7dc4a4022800a51fa4ffb564440e6f9
Alex Robey <arobey1@seas.upenn.edu>
2021-01-05 22:21:12 -0500

    updated launcher scripts

--- compass-gait/collect_data.sh
+++ compass-gait/collect_data.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 # Path and filename for saving dataset
-export SAVE_DATA_PATH=./
-export DATASET_NAME=rollouts.pkl
+export SAVE_DATA_PATH=./additive-noise
+export DATASET_NAME=level-0.25-pert-device-3.pkl
+
+# Compass gait parameters
+export HIP_MASS=10.0                # kg
+export LEG_MASS=5.0                 # kg
+export LEG_LENGTH=1.0               # m
+export LEG_CENTER_OF_MASS=0.5       # m
+export GRAVITY=9.81                 # m/s^2
+export SLOPE=0.0525                 # radians
 
 # Two kinds of rollouts can be collected:
 #   * 'iid': all rollouts are generated from iid initial conditions
@@ -12,10 +20,10 @@ export DATASET_NAME=rollouts.pkl
 export ROLLOUT_TYPE='pert'
 
 # Specifies number of rollouts and number of perturbed rollouts
-export N_ROLLOUTS=3
-export N_PERTURB_ROLLOUTS=1
+export N_ROLLOUTS=75
+export N_PERTURB_ROLLOUTS=2
 
-# Number of steps which define a 'success' or 'expert' trajecotry
+# Number of steps which define a 'success' or 'expert' trajectory
 export SUCCESS_N_STEPS=8
 
 # The nominal (e.g. expert) controller can be one of the following:
@@ -28,8 +36,16 @@ export NOMINAL_CTRL='energy'
 export HORIZON=750
 export DELTA_T=0.01
 
+# State noise level
+export NOISE_LEVEL=0.25
+
+export CUDA_VISIBLE_DEVICES=3
+
 python3 core/collect_data.py --save-data-path $SAVE_DATA_PATH \
     --dataset-name $DATASET_NAME --horizon $HORIZON --dt $DELTA_T \
     --n-rollouts $N_ROLLOUTS --n-pert-rollouts $N_PERTURB_ROLLOUTS \
     --nominal-ctrl $NOMINAL_CTRL --success-n-steps $SUCCESS_N_STEPS \
-    --fix-left 
+    --fix-left --rollout-type $ROLLOUT_TYPE \
+    --hip-mass $HIP_MASS --leg-mass $LEG_MASS \
+    --leg-length $LEG_LENGTH --leg-cent-of-mass $LEG_CENTER_OF_MASS \
+    --gravity $GRAVITY --slope $SLOPE
--- compass-gait/train.sh
+++ compass-gait/train.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
 # datasets
-export TRAINING_DATA=./big-dataset.pkl
+export TRAINING_DATA=./vary-hip-mass/all-trajs-shuffled.pkl
+# export TRAINING_DATA=./big-dataset.pkl
 export N_TRAIN_ROLLOUTS=500
 
 # directory to save all output plots and saved network parameters
-export RESULTS_DIR=./results
+export RESULTS_DIR=./robust-results-hip-0.1
+
+# Compass gait parameters
+export HIP_MASS=10.0                # kg
+export LEG_MASS=5.0                 # kg
+export LEG_LENGTH=1.0               # m
+export LEG_CENTER_OF_MASS=0.5       # m
+export GRAVITY=9.81                 # m/s^2
+export SLOPE=0.0525                 # radians
 
 # architecture and training hyperparameters
 export NET_DIMS=(4 32 16 1)
 export N_EPOCHS=30000
 
-# Lagrange multipliers/coefficients
+# training algorithm
+#   * 'primal': fix dual variables as hyperparameters and train by 
+#           minimizing the empirical Lagrangian
+#   * 'avg-primal-dual': use one dual variable for each type constraint
+#           and update dual variables via dual ascent
+#   * 'indiv-primal-dual': use one dual variable for each individual
+#           constraint and update all dual variables via dual ascent
+export train_mode='indiv-primal-dual'
+
+# Lagrange multipliers/coefficients (only used in 'primal' mode)
 export LAMBDA_SAFE=5.0
 export LAMBDA_UNSAFE=10.0
 export LAMBDA_CONT=0.2
@@ -19,7 +37,7 @@ export LAMBDA_DISCRETE=0.2
 export LAMBDA_GRAD=0.01
 export LAMBDA_PARAM=1.0
 
-# Loss margin hyperparameters
+# Loss margin hyperparameters (used in all three training modes)
 export GAMMA_SAFE=0.3
 export GAMMA_UNSAFE=0.3
 export GAMMA_CONT=0.05
@@ -27,9 +45,13 @@ export GAMMA_DISCRETE=0.05
 
 # neighbor sampling NUTS algorithm hyperparameters
 export MIN_NUM_NBRS=200
-export NBR_THRESH=0.04
+# export NBR_THRESH=0.04        # original HCBF
+export NBR_THRESH=0.07          # hip mass HCBF
+# export NBR_THRESH=0.05
 
-python core/train.py --neural-net-dims ${NET_DIMS[@]} --n-epochs $N_EPOCHS \
+export CUDA_VISIBLE_DEVICES=3
+
+python3 core/train.py --neural-net-dims ${NET_DIMS[@]} --n-epochs $N_EPOCHS \
     --train-data-path $TRAINING_DATA --n-train-rollouts $N_TRAIN_ROLLOUTS \
     --lam-safe $LAMBDA_SAFE --lam-unsafe $LAMBDA_UNSAFE \
     --lam-cnt $LAMBDA_CONT --lam-dis $LAMBDA_DISCRETE \
@@ -37,5 +59,8 @@ python core/train.py --neural-net-dims ${NET_DIMS[@]} --n-epochs $N_EPOCHS \
     --gam-safe $GAMMA_SAFE --gam-unsafe $GAMMA_UNSAFE \
     --gam-cnt $GAMMA_CONT --gam-dis $GAMMA_DISCRETE \
     --min-num-nbrs $MIN_NUM_NBRS --nbr-thresh $NBR_THRESH \
-    --results-dir $RESULTS_DIR --fix-left 
-    # --reload --reload-path ./final_hcbf.npy
+    --results-dir $RESULTS_DIR --fix-left --hip-mass $HIP_MASS \
+    --leg-mass $LEG_MASS --leg-length $LEG_LENGTH \
+    --leg-cent-of-mass $LEG_CENTER_OF_MASS \
+    --gravity $GRAVITY --slope $SLOPE \
+    --reload --reload-path robust-results-hip-0.1/trained_hcbf.npy
